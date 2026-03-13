feb6d2c9d0608aff3ac75f2702788d6ecd1bf341
Myo Zaw Win <david.hong199@gmail.com>
2026-03-13 21:55:06 +0700

    added password validation as well as name requirement for registering

--- apps/api/app/api/auth/register/route.ts
+++ apps/api/app/api/auth/register/route.ts
@@ -1,10 +1,26 @@
 import { createAdminClient } from "@/lib/supabase/server";
+import { validatePassword } from "@/lib/util";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
     const adminClient = createAdminClient();
+    if (!body.email || !body.password || !body.name) {
+      return NextResponse.json(
+        { error: "Email, password, and name are required." },
+        { status: 400 },
+      );
+    }
+
+    const { valid, errors } = validatePassword(body.password);
+
+    if (!valid) {
+      return NextResponse.json(
+        { error: `Invalid Password. ${errors.join(", ")}` },
+        { status: 400 },
+      );
+    }
 
     const { data, error } = await adminClient.auth.admin.createUser({
       email: body.email,
@@ -12,7 +28,6 @@ export async function POST(request: NextRequest) {
       email_confirm: true, // Automatically confirm the email for testing purposes
       user_metadata: {
         name: body.name,
-        avatar_url: body.avatar_url,
       },
     });
 
